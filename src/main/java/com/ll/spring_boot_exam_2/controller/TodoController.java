865bff76be7f8107d8796ceac66498d8c8b2ce33
taetae <dlaxodus1212@naver.com>
2024-08-14 15:46:54 +0900

    할일 수정, 삭제 추가

--- src/main/java/com/ll/spring_boot_exam_2/controller/TodoController.java
+++ src/main/java/com/ll/spring_boot_exam_2/controller/TodoController.java
@@ -1,9 +1,7 @@
 package com.ll.spring_boot_exam_2.controller;
 
 import com.ll.spring_boot_exam_2.domain.Todo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +22,17 @@ public class TodoController {
         return todos;
     }
 
+    @GetMapping("/detail/{id}") //가지고 있는 값들을 josn형태로 돌려준다.
+    public Todo getTodo(@PathVariable("id") long id) {
+        return todos
+                .stream() //여기서 하나 찾으면 리턴하고 없으면 null로 리턴을 해라
+                .filter(
+                        todo -> todo.getId() == id
+                ).findFirst()
+                .orElse(null);
+
+    }
+
     @GetMapping("/add")
     public Todo add(String body){
         Todo todo = Todo
@@ -33,6 +42,29 @@ public class TodoController {
                 .build();
         todos.add(todo);
         return todo;
+    }
+
+    @GetMapping("/remove/{id}")
+    public String removeTodo(@PathVariable("id") long id){
+        todos.removeIf(todo -> todo.getId() == id);
+
+        return id + "가 삭제되었습니다.";
+
+    }
+
+    @GetMapping("modify/{id}")
+    public boolean modifyTodo(@PathVariable("id") long id, String body){
+        Todo todo = todos.stream()
+                .filter(
+                        _todo -> _todo.getId() == id
+                )
+                .findFirst()
+                .orElse(null);
+
+        if(todo == null) return false;
+
+        todo.setBody(body);
+        return true;
     }
 
 
